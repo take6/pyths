83adc6ddaa4f1c6d933c5b4cdd0199600e913628
Takeshi Nakazato <takeshi.nakazato@nao.ac.jp>
2020-02-08 10:14:27 +0900

    get list of  start time

--- contentsparser.py
+++ contentsparser.py
@@ -1,4 +1,37 @@
-import selenium
+import collections
+
+
+class TimeRep(object):
+    def __init__(self, timestr):
+        self.__ts = timestr
+        h, m = self.__ts.split(':')
+        assert h.isdigit()
+        assert m.isdigit()
+        self.__ti = int(h) * 60 + int(m)
+
+    def asint(self):
+        return self.__ti
+
+    def __str__(self):
+        return self.__ts
+
+
+ProgramDescription = collections.namedtuple(
+    'ProgramDescription',
+    ['channel', 'start_time', 'summary']
+    )
+
+
+def element2description(element):
+    try:
+        time_element = element.find_element_by_class_name('time')
+    except Exception:
+        print('ERROR! <{} class="{}"'.format(element.tag_name, element.get_attribute('class')))
+    start_time = TimeRep(time_element.text)
+    channel = -1
+    summary = 'dummy'
+    return ProgramDescription(channel=channel, start_time=start_time, summary=summary)
+    #return element
 
 
 def get_program_table(html_element):
@@ -18,5 +51,12 @@ def get_program_table(html_element):
     return tables[-1]
 
 
-def get_program_list(program_table):
-    pass
+def filter_cell(cell):
+    print(cell.tag_name)
+    return cell.tag_name == 'td'
+
+
+def gen_programs(program_table):
+    for cell in filter(filter_cell, program_table.find_elements_by_class_name('turnup')):
+        program = cell.find_element_by_class_name('detail')
+        yield element2description(program)
