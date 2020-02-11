0dcfbc3e10e70aa04cd8bacff7dabb2dc24be727
take6 <umechiki_0512@yahoo.co.jp>
2020-02-12 08:09:35 +0900

    filter out early programs of next day

--- datahandler.py
+++ datahandler.py
@@ -1,4 +1,5 @@
 import collections
+import numpy
 
 
 def classify_by_channel(records):
@@ -6,8 +7,21 @@ def classify_by_channel(records):
     for record in records:
         programs_by_channel[record.station].append(record)
 
+    for station in programs_by_channel.keys():
+        v = programs_by_channel[station]
+        programs_by_channel[station] = filter_by_time(v)
+
     return programs_by_channel
 
 
-def sort_by_time(records):
-    pass
+def filter_by_time(records):
+    start_times = numpy.asarray(
+        [record.start_time.asint() for record in records]
+    )
+    dt = start_times[1:] - start_times[:-1]
+    i = numpy.where(dt < 0)[0]
+    if len(i) == 0:
+        return records
+    else:
+        return records[:i[0] + 1]
+
