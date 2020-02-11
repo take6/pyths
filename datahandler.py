8b5a83e4d46313d3405d68374bc116523fa3a442
take6 <umechiki_0512@yahoo.co.jp>
2020-02-11 16:34:05 +0900

    classify ProgramDescription by station

--- datahandler.py
+++ datahandler.py
@@ -1,8 +1,12 @@
-import collectins
+import collections
 
 
 def classify_by_channel(records):
-    pass
+    programs_by_channel = collections.defaultdict(list)
+    for record in records:
+        programs_by_channel[record.station.channel].append(record)
+
+    return programs_by_channel
 
 
 def sort_by_time(records):
