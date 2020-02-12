bac48189cc420d3f9be4aa6a2b037df660683a3f
take6 <umechiki_0512@yahoo.co.jp>
2020-02-13 08:03:26 +0900

    calculate duration

--- datahandler.py
+++ datahandler.py
@@ -1,6 +1,8 @@
 import collections
 import numpy
 
+from .contentsparser import ProgramDescription
+
 
 def classify_by_channel(records):
     programs_by_channel = collections.defaultdict(list)
@@ -9,7 +11,7 @@ def classify_by_channel(records):
 
     for station in programs_by_channel.keys():
         v = programs_by_channel[station]
-        programs_by_channel[station] = filter_by_time(v)
+        programs_by_channel[station] = add_duration(filter_by_time(v))
 
     return programs_by_channel
 
@@ -25,3 +27,32 @@ def filter_by_time(records):
     else:
         return records[:i[0] + 1]
 
+
+def set_duration(record, duration):
+    return ProgramDescription(
+        station=record.station,
+        start_time=record.start_time,
+        duration=duration,
+        title=record.title,
+        summary=record.summary
+    )
+
+
+def add_duration(records):
+    for i in range(len(records) - 2):
+        prog_now = records[i]
+        prog_next = records[i + 1]
+        start = prog_now.start_time.asint()
+        end = prog_next.start_time.asint()
+        assert start <= end
+        duration = end - start
+        records[i] = set_duration(prog_now, duration)
+
+    # set dummy duration for the last record
+    start = records[-1].start_time.asint()
+    duration = start % 60
+    if duration == 0:
+        duration = 60
+    records[-1] = set_duration(records[-1], duration)
+
+    return records
