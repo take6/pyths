9ef9a52f08e4a171fe6563ee6a15514a4cb09dbf
take6 <umechiki_0512@yahoo.co.jp>
2020-03-20 07:14:41 +0900

    define --release option
    currently it only affects "zip_safe" option

--- setup.py
+++ setup.py
@@ -1,8 +1,15 @@
 import setuptools
+import sys
+
+if '--release' in sys.argv:
+    zipsafe = True
+else:
+    zipsafe = False
 
 setuptools.setup(
     name='pyths',
     version='0.0.0',
     description='2H Suspense Drama',
-    packages=setuptools.find_packages()
+    packages=setuptools.find_packages(),
+    zip_safe=zipsafe
 )
