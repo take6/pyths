017d19ea37dd6e6094bafdc0dc59d932c43e2da5
take6 <umechiki_0512@yahoo.co.jp>
2020-05-23 14:45:17 +0900

    API change for ml
    - define main

--- pyths/ml/__init__.py
+++ pyths/ml/__init__.py
@@ -3,3 +3,10 @@ from .loader import load_data, load_model
 from .preprocessor import preprocess
 
 from .naivebayes import NaiveBayes
+
+
+def main(csvdata, modeldata):
+    df = load_data(csvdata)
+    machine = load_model(modeldata)
+    categorized = machine.categorize(df)
+    save_data(categorized, csvdata, overwrite=True)
